02cf7190708a298170f91984385185616a897b3b
Arya Yunanta <aryayun90@gmail.com>
2021-05-06 11:40:45 +0700

    Create jar file

--- src/test/java/aryahmph/repository/MahasiswaRepositoryImplTest.java
+++ src/test/java/aryahmph/repository/MahasiswaRepositoryImplTest.java
@@ -1,75 +1,75 @@
-package aryahmph.repository;
-
-import aryahmph.entity.Mahasiswa;
-import aryahmph.util.DatabaseUtil;
-import com.zaxxer.hikari.HikariDataSource;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-
-public class MahasiswaRepositoryImplTest {
-  private HikariDataSource dataSource;
-  private MahasiswaRepository mahasiswaRepository;
-
-  @BeforeEach
-  void setUp() {
-    dataSource = DatabaseUtil.getDataSource();
-    mahasiswaRepository = new MahasiswaRepositoryImpl(dataSource);
-  }
-
-  @Test
-  void testAdd() {
-    Mahasiswa mahasiswa = new Mahasiswa("Tamir Gading", "09082025009", "gading@test.com");
-    mahasiswaRepository.add(mahasiswa);
-  }
-
-  @Test
-  void testRemove() {
-    boolean delete = mahasiswaRepository.remove(5);
-    Assertions.assertEquals(true, delete);
-  }
-
-  @Test
-  void testAddAndRemove() {
-    mahasiswaRepository.remove(2);
-    Mahasiswa mahasiswa = new Mahasiswa("Arya Yunanta", "09021182025009", "arya@test.com");
-    mahasiswaRepository.add(mahasiswa);
-  }
-
-  @Test
-  void testFindAll() {
-    List<Mahasiswa> list = mahasiswaRepository.findAll();
-    for (Mahasiswa mahasiswa : list) {
-      System.out.println(mahasiswa);
-    }
-  }
-
-  @Test
-  void testFindById() {
-    Mahasiswa mahasiswa = mahasiswaRepository.findById(2);
-    System.out.println(mahasiswa);
-  }
-
-  @Test
-  void testFind() {
-    List<Mahasiswa> mahasiswaList = mahasiswaRepository.find("arya");
-    for (Mahasiswa mahasiswa : mahasiswaList) {
-      System.out.println(mahasiswa);
-    }
-  }
-
-  @Test
-  void testUpdate() {
-    Mahasiswa mahasiswa = new Mahasiswa(100,"Arya Pradata", "023013010", "arya@test.com");
-    Assertions.assertFalse(mahasiswaRepository.update(mahasiswa));
-  }
-
-  @AfterEach
-  void tearDown() {
-    dataSource.close();
-  }
-}
+//package aryahmph.repository;
+//
+//import aryahmph.entity.Mahasiswa;
+//import aryahmph.util.DatabaseUtil;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.List;
+//
+//
+//public class MahasiswaRepositoryImplTest {
+//  private HikariDataSource dataSource;
+//  private MahasiswaRepository mahasiswaRepository;
+//
+//  @BeforeEach
+//  void setUp() {
+//    dataSource = DatabaseUtil.getDataSource();
+//    mahasiswaRepository = new MahasiswaRepositoryImpl(dataSource);
+//  }
+//
+//  @Test
+//  void testAdd() {
+//    Mahasiswa mahasiswa = new Mahasiswa("Tamir Gading", "09082025009", "gading@test.com");
+//    mahasiswaRepository.add(mahasiswa);
+//  }
+//
+//  @Test
+//  void testRemove() {
+//    boolean delete = mahasiswaRepository.remove(5);
+//    Assertions.assertEquals(true, delete);
+//  }
+//
+//  @Test
+//  void testAddAndRemove() {
+//    mahasiswaRepository.remove(2);
+//    Mahasiswa mahasiswa = new Mahasiswa("Arya Yunanta", "09021182025009", "arya@test.com");
+//    mahasiswaRepository.add(mahasiswa);
+//  }
+//
+//  @Test
+//  void testFindAll() {
+//    List<Mahasiswa> list = mahasiswaRepository.findAll();
+//    for (Mahasiswa mahasiswa : list) {
+//      System.out.println(mahasiswa);
+//    }
+//  }
+//
+//  @Test
+//  void testFindById() {
+//    Mahasiswa mahasiswa = mahasiswaRepository.findById(2);
+//    System.out.println(mahasiswa);
+//  }
+//
+//  @Test
+//  void testFind() {
+//    List<Mahasiswa> mahasiswaList = mahasiswaRepository.find("arya");
+//    for (Mahasiswa mahasiswa : mahasiswaList) {
+//      System.out.println(mahasiswa);
+//    }
+//  }
+//
+//  @Test
+//  void testUpdate() {
+//    Mahasiswa mahasiswa = new Mahasiswa(100,"Arya Pradata", "023013010", "arya@test.com");
+//    Assertions.assertFalse(mahasiswaRepository.update(mahasiswa));
+//  }
+//
+//  @AfterEach
+//  void tearDown() {
+//    dataSource.close();
+//  }
+//}
--- src/test/java/aryahmph/service/MahasiswaServiceImplTest.java
+++ src/test/java/aryahmph/service/MahasiswaServiceImplTest.java
@@ -1,52 +1,52 @@
-package aryahmph.service;
-
-import aryahmph.repository.MahasiswaRepositoryImpl;
-import aryahmph.util.DatabaseUtil;
-import com.zaxxer.hikari.HikariDataSource;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-public class MahasiswaServiceImplTest {
-
-  private HikariDataSource dataSource;
-  private MahasiswaService mahasiswaService;
-
-  @BeforeEach
-  void setUp() {
-    dataSource = DatabaseUtil.getDataSource();
-    mahasiswaService = new MahasiswaServiceImpl(new MahasiswaRepositoryImpl(dataSource));
-  }
-
-  @Test
-  void testAdd() {
-    mahasiswaService.addMahasiswa("Gandi Subara", "090211820", "gandi@test.com");
-  }
-
-  @Test
-  void testRemove() {
-    mahasiswaService.removeMahasiswa(2);
-  }
-
-  @Test
-  void testShowMahasiswa() {
-    mahasiswaService.showMahasiswa();
-  }
-
-  @Test
-  void testSearchMahasiswa() {
-    mahasiswaService.searchMahasiswa("arya");
-  }
-
-  @Test
-  void testUpdateMahasiswa() {
-    mahasiswaService.updateMahasiswa(1,"Arya Yunanta", "09120313091", "gandi@test.com");
-  }
-
-  @AfterEach
-  void tearDown() {
-    dataSource.close();
-  }
-
-}
+//package aryahmph.service;
+//
+//import aryahmph.repository.MahasiswaRepositoryImpl;
+//import aryahmph.util.DatabaseUtil;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//public class MahasiswaServiceImplTest {
+//
+//  private HikariDataSource dataSource;
+//  private MahasiswaService mahasiswaService;
+//
+//  @BeforeEach
+//  void setUp() {
+//    dataSource = DatabaseUtil.getDataSource();
+//    mahasiswaService = new MahasiswaServiceImpl(new MahasiswaRepositoryImpl(dataSource));
+//  }
+//
+//  @Test
+//  void testAdd() {
+//    mahasiswaService.addMahasiswa("Gandi Subara", "090211820", "gandi@test.com");
+//  }
+//
+//  @Test
+//  void testRemove() {
+//    mahasiswaService.removeMahasiswa(2);
+//  }
+//
+//  @Test
+//  void testShowMahasiswa() {
+//    mahasiswaService.showMahasiswa();
+//  }
+//
+//  @Test
+//  void testSearchMahasiswa() {
+//    mahasiswaService.searchMahasiswa("arya");
+//  }
+//
+//  @Test
+//  void testUpdateMahasiswa() {
+//    mahasiswaService.updateMahasiswa(1,"Arya Yunanta", "09120313091", "gandi@test.com");
+//  }
+//
+//  @AfterEach
+//  void tearDown() {
+//    dataSource.close();
+//  }
+//
+//}
